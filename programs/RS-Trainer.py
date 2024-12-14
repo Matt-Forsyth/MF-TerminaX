b6bdc3af33cecafe4b73f189466f6b938ac2a999
Matt-Forsyth <matthew.forsyth01@gmail.com>
2024-12-13 18:19:21 -0800

    Changed Paths to reflect github clone

--- programs/RS-Trainer.py
+++ programs/RS-Trainer.py
@@ -6,14 +6,14 @@ if __name__ == "__main__":
 
     # Train the model
     model.train(
-        data="/media/matthew/USB-Hub/MF-Repo/TerminaX/Input/data.yaml",  # Path to your dataset YAML file
+        data="/media/matthew/USB-Hub/MF-TerminaX/Input/data.yaml",  # Path to your dataset YAML file
         epochs=100,                 # Number of training epochs
         imgsz=640,                 # Image size for training (default: 640)
         batch=16,                  # Batch size (adjust based on your system capabilities)
         device=0,                  # Use '0' for GPU, 'cpu' for CPU training
         workers=4,                 # Number of data loader workers (reduce to 0 if issues persist)
         project="Output/RS-Model",  # Project directory to save results
-        name="RS-Model"   # Run name (for tracking in saved results)
+        name="RoadScout"   # Run name (for tracking in saved results)
     )
 
     # Evaluate the trained model
